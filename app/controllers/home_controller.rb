3b51da40d0d5e66b748c12340f834bdc9e1457e3
Ayush Vaidya <ayush.everest@gmail.com>
2016-11-17 20:30:28 -0500

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+	def index
+	end
+
+	def messages
+	end
+
+	def tenant_requests
+	end
+
+	def payments
+	end
+
+	def documents
+	end
+
+	def landing
+		render :layout => "landing"
+	end
+
+end
